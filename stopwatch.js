2ae2045b9a08e223a75df1333efaefbf3e3b6b00
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-03-07 19:43:45 +0600

    project done

---
+++ stopwatch.js
@@ -0,0 +1,40 @@
+let [hours, minutes, seconds] = [0, 0, 0];
+const displayTime = document.getElementById('display-time');
+let timer = null;
+
+function stopwatch(){
+    seconds++
+    if(seconds == 60){
+        seconds = 0;
+        minutes++
+        if(minutes == 60){
+            minutes = 0;
+            hours++
+            if(hours > 12){
+                hours = 0;
+            }
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerText = h + " : " + m + " : " + s;
+}
+
+function watchStart(){
+    if(timer !== null){
+        clearInterval(timer)
+    }
+    timer = setInterval(stopwatch, 1000)
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
+
+function watchReset(){
+    clearInterval(timer);
+    [hours, minutes, seconds] = [0, 0, 0];
+    displayTime.innerText = "00 : 00 : 00";
+}
